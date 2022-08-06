e81f141ad6d6bc808acb7b43be18063df311dc8c
lisovenkoNastya <lisovenko.ad@gmail.com>
2022-08-06 13:57:56 +0300

    Fix MS_IN_HOUR definition

--- src/components/tickets/TicketCard.vue
+++ src/components/tickets/TicketCard.vue
@@ -1,18 +1,14 @@
 <template>
   <app-sheet class="ticket-card shadow-3">
     <div class="ticket-card__top flex items-center justify-between mb2">
-      <div class="ticket-card__price h3 line-height-1" v-format-number>
-        {{ ticketData.price }} Р
-      </div>
+      <div class="ticket-card__price h3 line-height-1" v-format-number>{{ ticketData.price }} Р</div>
       <div class="ticket-card__price">
         <img class="block" :src="currentCompanyLogo" />
       </div>
     </div>
     <div class="ticket-card__bottom flex items-center justify-between">
       <div class="ticket-card__route col-4">
-        <div class="subheading text-muted">
-          {{ ticketData.info.origin }} – {{ ticketData.info.destination }}
-        </div>
+        <div class="subheading text-muted">{{ ticketData.info.origin }} – {{ ticketData.info.destination }}</div>
         <div>10:45 – 08:00</div>
       </div>
       <div class="ticket-card__duration col-4">
@@ -21,9 +17,7 @@
       </div>
       <div class="ticket-card__stops col-4">
         <template v-if="ticketData.info.stops.length">
-          <div class="subheading text-muted">
-            {{ ticketData.info.stops.length }} пересадки
-          </div>
+          <div class="subheading text-muted">{{ ticketData.info.stops.length }} пересадки</div>
           <div>{{ ticketData.info.stops.join(', ') }}</div>
         </template>
         <div v-else class="subheading text-muted">Без пересадок</div>
@@ -39,7 +33,7 @@ import AppSheet from '@/components/ui/AppSheet.vue';
 import useComapanies from '@/composables/useCompanies';
 import vFormatNumber from '@/directives/vFormatNumber';
 
-const MS_IN_HOUR = 3600000;
+const MS_IN_HOUR = 1000 * 60 * 60;
 
 interface TicketCardProps {
   ticketData: Ticket;
@@ -48,9 +42,7 @@ const props = defineProps<TicketCardProps>();
 const { ticketData } = toRefs(props);
 const { getCompanyLogo } = useComapanies();
 
-const currentCompanyLogo = computed(() =>
-  getCompanyLogo(ticketData.value.companyId),
-);
+const currentCompanyLogo = computed(() => getCompanyLogo(ticketData.value.companyId));
 </script>
 
 <style scoped lang="scss">

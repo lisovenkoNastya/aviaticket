2f7645fc4478a69b7e59e40be1fa63d1ac825d37
lisovenkoNastya <lisovenko.ad@gmail.com>
2022-08-13 12:58:02 +0400

    Typify api methods

--- src/api/companyApi.ts
+++ src/api/companyApi.ts
@@ -1,6 +1,6 @@
+import { Company } from '@/interfaces/Company';
 import httpClient from './httpClient';
 
-const fetchCompanies = () =>
-  httpClient.get('/a1b1c28b32d9785bb26c').then((resp) => resp.data);
+const fetchCompanies = (): Promise<Company[]> => httpClient.get('/a1b1c28b32d9785bb26c').then((resp) => resp.data);
 
 export default { fetchCompanies };
